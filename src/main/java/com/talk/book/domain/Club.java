7ce3bca880d237511c7e26ca98aa5593e216af74
jsky516 <gksdlqkek@gmail.com>
2025-03-11 20:46:50 +0900

    feat: 로그인 기능 구현

--- src/main/java/com/talk/book/domain/Club.java
+++ src/main/java/com/talk/book/domain/Club.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "club")
 @Getter
@@ -14,6 +16,40 @@ public class Club {
     @Column(name = "club_id")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "host_id", nullable = false)
+    private Member host;
 
+    @Column(length = 20, nullable = false)
+    private String isbn13;
 
+    @Column(length = 255, nullable = false)
+    private String name;
+
+    @Column(length = 255, nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private int maxParticipants;
+
+    @Column(nullable = false)
+    private int currentParticipant = 0;
+
+    @Column(length = 20, nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @Column(columnDefinition = "TEXT")
+    private String clubDescription;
+
+    @Column(length = 255)
+    private String clubImage;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
--- src/main/java/com/talk/book/repository/ClubRepository.java
+++ src/main/java/com/talk/book/repository/ClubRepository.java
@@ -1,4 +1,9 @@
-//package com.talk.book.repository;
-//
-//public interface ClubRepository {
-//}
+package com.talk.book.repository;
+
+import com.talk.book.domain.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClubRepository extends JpaRepository<Club, Long> {
+}
