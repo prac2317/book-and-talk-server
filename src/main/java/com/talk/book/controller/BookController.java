094fb1af939f8076c589fb742847c81b77442dfd
prac2317 <lowdy92@naver.com>
2025-03-13 09:45:08 +0900

    feat: DataInitializer로 초기 사용자 데이터 추가

---
+++ src/main/java/com/talk/book/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.talk.book.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BookController {
+
+    public void getAladinList() {
+        // todo: 알라딘으로 api 요청하기
+
+        // todo: 받아온 리스트 책에 저장하기
+    }
+}
