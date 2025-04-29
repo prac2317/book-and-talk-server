bcc5064d1d9182f573a0ab42da1cb0aa2cfcda0c
prac2317 <lowdy92@naver.com>
2025-04-29 12:20:23 +0900

    chore: 배포를 위한 임시 쿠키 설정

--- src/main/java/com/talk/book/security/WebConfig.java
+++ src/main/java/com/talk/book/security/WebConfig.java
@@ -9,9 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOriginPatterns("https://book-and-talk-client-2nurgfakp-sjs-projects-c78fe08f.vercel.app/")
+//                .allowedOrigins("http://localhost:5173")
+//                .allowedOrigins("https://book-and-talk-client-2nurgfakp-sjs-projects-c78fe08f.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
